a0490042b80be8cc3937d43c98a13036f3682701
Herobrine-2 <parfumsisevepipi0@gmail.com>
2026-08-13 17:38:44 +0300

    new skınss

--- MAGAZADAN_MOD_SIL.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 > nul
-title Skynix Magaza - Mod Sil
-cd /d "%~dp0"
-node magaza-sil.js
-pause
--- MAGAZAYA_MOD_EKLE.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001 > nul
-title Skynix Magaza - Mod Ekle
-cd /d "%~dp0"
-node magaza-ekle.js
-pause
